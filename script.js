c5f5ede65f306eeef422002148e43cdc7ea5ee03
eavargasb <eduardovargas3117@gmail.com>
2024-07-20 22:07:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Añade un mensaje personalizado al cargar la página
+window.onload = function() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = 'gracias por tanto, a veces no creo mi suerte contigo, ojala me dures toda!! la vida!';
+};
